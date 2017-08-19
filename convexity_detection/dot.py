cec88f6ad9069c9302be8a912020470a9e3927ea
Francesco Ceccon <francesco@ceccon.me>
2017-08-19 13:41:17 +0100

    Generate DOT graph of expression

---
+++ convexity_detection/dot.py
@@ -0,0 +1,84 @@
+from numbers import Number
+from convexity_detection.expr_visitor import ExprVisitor
+import pyomo.core.base.expr_pyomo4 as omo
+from pyomo.core.base.var import SimpleVar
+import numpy as np
+
+
+class DotVisitor(ExprVisitor):
+    def __init__(self, outfile):
+        self._f = outfile
+        self._visited = set()
+        self.write_header()
+
+    def write_header(self):
+        self._f.write('digraph G {\n')
+
+    def write_node(self, node, label):
+        if id(node) in self._visited:
+            return
+        self._visited.add(id(node))
+        self._f.write('    {} [label="{}"];\n'.format(id(node), label))
+
+    def write_coef_node(self, node, coef):
+        if np.isclose(coef, 1.0):
+            return node
+        node_id = 'c{}'.format(node)
+        node_prod_id = 'p{}'.format(node)
+        self._f.write('    {} [label="{:.4f}"];\n'.format(node_id, coef))
+        self._f.write('    {} [label="*"];\n'.format(node_prod_id))
+        self._f.write('    {} -> {};\n'.format(node_prod_id, node))
+        self._f.write('    {} -> {};\n'.format(node_prod_id, node_id))
+        return node_prod_id
+
+    def write_arc(self, from_, to):
+        if isinstance(to, Number):
+            self._f.write('    constant{0} [label="{0}"];\n'.format(to))
+            self._f.write('    {} -> constant{};\n'.format(id(from_), to))
+        else:
+            self._f.write('    {} -> {};\n'.format(id(from_), id(to)))
+
+    def visit_end(self):
+        self._f.write('}\n')
+
+    def visit_product(self, lvl, e):
+        self.write_node(e, '*')
+        for a in e._args:
+            self.write_arc(e, a)
+
+    def visit_division(self, lvl, e):
+        self.write_node(e, '/')
+        for a in e._args:
+            self.write_arc(e, a)
+
+    def visit_linear(self, lvl, e):
+        self.write_node(e, '+')
+
+        for node, coef in e._coef.items():
+            node_id = self.write_coef_node(node, coef)
+            self._f.write('    {} -> {};\n'.format(id(e), node_id))
+
+        if not np.isclose(e._const, 0):
+            self._f.write('    const{} [label="{:.4f}"];\n'.format(id(e), e._const))
+            self._f.write('    {0} -> const{0};\n'.format(id(e)))
+
+    def visit_sum(self, lvl, e):
+        self.write_node(e, '+')
+        for a in e._args:
+            self.write_arc(e, a)
+
+    def visit_simple_var(self, lvl, v):
+        self.write_node(v, v.name)
+
+    def visit_unary_function(self, lvl, e):
+        self.write_node(e, e._name)
+        for a in e._args:
+            self.write_arc(e, a)
+
+    def visit_negation(self, lvl, e):
+        self.write_node(e, 'neg')
+        for a in e._args:
+            self.write_arc(e, a)
+
+    def visit_number(self, lvl, n):
+        print(n)
---
+++ tests/test_dot.py
@@ -0,0 +1,30 @@
+import pytest
+import io
+import pyomo.environ as aml
+from convexity_detection.dot import *
+from convexity_detection.pyomo_compat import *
+import numpy as np
+import pyomo.core.base.expr_pyomo4 as omo
+
+
+set_pyomo4_expression_tree()
+
+
+def test_dot():
+    x = aml.Var()
+    y = aml.Var()
+    z = aml.Var()
+
+    # e0 = 2*x*(3*y + z)
+    with omo.bypass_clone_check():
+        p = x + 1
+        e0 = p * aml.log(p)
+    # e0 = 7*(aml.sin(x/y) + x/y - aml.exp(y))*(x/y - aml.exp(y))
+
+    f = io.StringIO()
+    v = DotVisitor(f)
+    v.visit(e0)
+    print(e0)
+
+    print(f.getvalue())
+    assert False
