dc2cfe1406a412bec8707b32fd8bbc332968a156
Francesco Ceccon <francesco@ceccon.me>
2017-08-19 13:40:33 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='convexity_detection',
+    author='Francesco Ceccon',
+    author_email='francesco@ceccon.me',
+    packages=find_packages('convexity_detection'),
+    requires=['pyomo'],
+    setup_requires=['pytest-runner'],
+    tests_requires=['pytest'],
+)
