a5ba2476605a3718910cc3524beb64e94d9a5754
Francesco Ceccon <francesco@ceccon.me>
2017-12-13 22:44:43 +0000

    Add Dockerfile and instructions to run as AWS Batch job
    Running `model_summary.py` on a big dataset (e.g. MINLPLib2) requires
    tens of hours of computation time.
    By dockerizing the script, we can run it in parallel on the cloud
    and cut the wall clock time to a few hours.

--- README.md
+++ README.md
@@ -19,27 +19,18 @@ Usage
 -----
 
 The package contains an utility to display structure information about
-a single problem or, in batch mode, a list of problems.
+a single problem.
 
 You can run the utility as:
 
-    model_summary.py -i /path/to/problem.osil
+    model_summary.py -p /path/to/problem.osil
 
 or, if you want to check variables bounds include the solution:
 
-    model_summary.py -i /path/to/problem.osil:/path/to/problem.sol
+    model_summary.py -p /path/to/problem.osil -s /path/to/problem.sol
 
-
-If you want to run the utility on a large set of problem, you can use batch mode:
-
-    model_summary.py --batch -i /path/to/file/list.txt -o /path/to/output.csv
-
-Where the input file is like:
-
-    /path/to/problem1.osil:/path/to/problem1.sol
-    /path/to/problem2.osil:/path/to/probelm2.sol
-    ...
-
+The repository also includes a `Dockerfile` to simplify running the utility in
+batch mode in a cloud environment. Refer to the `batch` folder for more information.
 
 
 References
---
+++ batch/aws_batch_job.py
@@ -0,0 +1,67 @@
+import argparse
+import boto3
+import os
+import datetime
+
+
+batch = boto3.client('batch')
+s3 = boto3.client('s3')
+
+
+def problem_name_from_path(path):
+    basename = os.path.basename(path)
+    return os.path.splitext(basename)[0]
+
+
+def list_input_problems(bucket):
+    paginator = s3.get_paginator('list_objects')
+    for page in paginator.paginate(Bucket=args.input_bucket, Prefix='osil/'):
+        for input_problem in page['Contents']:
+            name = input_problem['Key']
+            if name.endswith('.osil'):
+                yield name
+
+
+def get_sol(bucket, problem_name):
+    sol_name = 'sol/' + problem_name + '.p1.sol'
+    try:
+        res = s3.head_object(Bucket=bucket, Key=sol_name)
+        return sol_name
+    except:
+        return None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-bucket', required=True)
+    parser.add_argument('--output-bucket', required=True)
+    parser.add_argument('--job-definition', required=True)
+    parser.add_argument('--job-queue', required=True)
+    args = parser.parse_args()
+
+    problems = list_input_problems(args.input_bucket)
+    for problem in problems:
+        problem_name = problem_name_from_path(problem)
+        sol = get_sol(args.input_bucket, problem_name)
+        now = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        jobname = 'minlplib2-' + problem_name + '-' + now
+        if sol is not None:
+            command = [
+                '-p', 's3://' + args.input_bucket + '/' + problem,
+                '-s', 's3://' + args.input_bucket + '/' + sol,
+                '-o', 's3://' + args.output_bucket + '/' + problem_name + '.json'
+            ]
+        else:
+            command = [
+                '-p', 's3://' + args.input_bucket + '/' + problem,
+                '-o', 's3://' + args.output_bucket + '/' + problem_name + '.json'
+            ]
+        print('Submitting job: {}'.format(command))
+        batch.submit_job(
+            jobName=jobname,
+            jobQueue=args.job_queue,
+            jobDefinition=args.job_definition,
+            containerOverrides={
+                'command': command
+            }
+        )
