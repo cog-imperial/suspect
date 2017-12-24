be5188b9a45cfb1faf446958b5ee861f0f43cbb0
Francesco Ceccon <francesco@ceccon.me>
2017-12-24 14:20:31 +0000

    Add script to produce csv of results

---
+++ batch/result_to_csv.py
@@ -0,0 +1,33 @@
+import argparse
+import boto3
+import json
+import pandas as pd
+
+
+s3 = boto3.client('s3')
+
+
+def list_objects(bucket):
+    paginator = s3.get_paginator('list_objects')
+    for page in paginator.paginate(Bucket=bucket):
+        for problem in page['Contents']:
+            yield problem['Key']
+
+
+def read_problem(bucket, problem):
+    res = s3.get_object(Bucket=bucket, Key=problem)
+    body = res['Body'].read()
+    if len(body) > 0:
+        return json.loads(body)
+    return None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bucket')
+    args = parser.parse_args()
+
+    results = filter(None, [
+        read_problem(args.bucket, problem)
+        for problem in list_objects(args.bucket)
+    ])
