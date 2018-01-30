da665381359cc15e919c123737148c594aa4eb4d
Francesco Ceccon <francesco@ceccon.me>
2018-01-30 13:14:49 +0000

    Exclude dag.dot and pure abstract classes from coverage

--- suspect/bound/bound.py
+++ suspect/bound/bound.py
@@ -18,11 +18,11 @@ import warnings
 
 class Bound(metaclass=abc.ABCMeta):
     @abc.abstractproperty
-    def lower_bound(self):
+    def lower_bound(self):  # pragma: no cover
         pass
 
     @property
-    def l(self):
+    def l(self):  # pragma: no cover
         warnings.warn(
             "Bound.l is deprecated. Use Bound.lower_bound",
             DeprecationWarning,
@@ -30,11 +30,11 @@ class Bound(metaclass=abc.ABCMeta):
         return self.lower_bound
 
     @abc.abstractproperty
-    def upper_bound(self):
+    def upper_bound(self):  # pragma: no cover
         pass
 
     @property
-    def u(self):
+    def u(self):  # pragma: no cover
         warnings.warn(
             "Bound.u is deprecated. Use Bound.upper_bound",
             DeprecationWarning,
@@ -42,87 +42,87 @@ class Bound(metaclass=abc.ABCMeta):
         return self.upper_bound
 
     @abc.abstractmethod
-    def is_zero(self):
+    def is_zero(self):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def is_positive(self):
+    def is_positive(self):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def is_negative(self):
+    def is_negative(self):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def tighten(self, other):
+    def tighten(self, other):  # pragma: no cover
         """Returns a new bound which is the tightest intersection of bounds."""
         pass
 
     @abc.abstractmethod
-    def add(self, other):
+    def add(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def sub(self, other):
+    def sub(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def mul(self, other):
+    def mul(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def div(self, other):
+    def div(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def equals(self, other):
+    def equals(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def contains(self, other):
+    def contains(self, other):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def zero(self):
+    def zero(self):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def is_nonpositive(self):
+    def is_nonpositive(self):  # pragma: no cover
         pass
 
     @abc.abstractmethod
-    def is_nonnegative(self):
+    def is_nonnegative(self):  # pragma: no cover
         pass
 
-    def __add__(self, other):
+    def __add__(self, other):  # pragma: no cover
         return self.add(other)
 
-    def __radd__(self, other):
+    def __radd__(self, other):  # pragma: no cover
         return self.__add__(other)
 
-    def __sub__(self, other):
+    def __sub__(self, other):  # pragma: no cover
         return self.sub(other)
 
-    def __neg__(self):
+    def __neg__(self):  # pragma: no cover
         return self.zero() - self
 
-    def __mul__(self, other):
+    def __mul__(self, other):  # pragma: no cover
         return self.mul(other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other):  # pragma: no cover
         return self.__mul__(other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other):  # pragma: no cover
         return self.div(other)
 
-    def __eq__(self, other):
+    def __eq__(self, other):  # pragma: no cover
         return self.equals(other)
 
-    def __contains__(self, other):
+    def __contains__(self, other):  # pragma: no cover
         return self.contains(other)
 
-    def __repr__(self):
+    def __repr__(self):  # pragma: no cover
         return '<{} at {}>'.format(str(self), id(self))
 
-    def __str__(self):
+    def __str__(self):  # pragma: no cover
         return '[{}, {}]'.format(self.lower_bound, self.upper_bound)
