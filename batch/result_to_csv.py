25f9903d4f27c00500e78a9c94d1a3e038680376
Francesco Ceccon <francesco@ceccon.me>
2018-02-10 21:36:01 +0000

    Add option to specify prefix when downloading results

--- batch/result_to_csv.py
+++ batch/result_to_csv.py
@@ -7,9 +7,9 @@ import pandas as pd
 s3 = boto3.client('s3')
 
 
-def list_objects(bucket):
+def list_objects(bucket, prefix):
     paginator = s3.get_paginator('list_objects')
-    for page in paginator.paginate(Bucket=bucket):
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
         for problem in page['Contents']:
             yield problem['Key']
 
@@ -26,11 +26,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('bucket')
     parser.add_argument('output')
+    parser.add_argument('--prefix', type=str, default='')
     args = parser.parse_args()
 
     results = filter(None, [
         read_problem(args.bucket, problem)
-        for problem in list_objects(args.bucket)
+        for problem in list_objects(args.bucket, args.prefix)
     ])
     dataframe = pd.DataFrame.from_records(results)
     dataframe.to_csv(args.output, index=False)
