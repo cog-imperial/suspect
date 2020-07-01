51a15003f8297c55afac0e883298522bb6e991d3
Francesco Ceccon <francesco@ceccon.me>
2020-07-01 10:15:49 +0100

    version bump to 2.0.1

--- suspect/__version__.py
+++ suspect/__version__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __author__ = 'Francesco Ceccon'
 __author_email__ = 'francesco@ceccon.me'
 __license__ = 'Apache 2.0'
